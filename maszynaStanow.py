086111b6d864d40efbe2329e38f1ddebfdc4998a
kfigon <kamil.figon@jitsolutions.pl>
2018-02-12 17:26:21 +0100

    poczatek maszyny stanow. Infrastruktura zrobiona

--- maszynaStanow.py
+++ maszynaStanow.py
@@ -6,13 +6,25 @@ class MaszynaStanow:
         self.__stany={}
 
         ileStanow = 2**self.__rej.getDlugoscRejestru()
-        szablon = '0%db' % ileZmiennych
+        szablon = '0%db' % self.__rej.getDlugoscRejestru()
         for i in range(ileStanow):
             self.__rej.reset()
             liczba = format(i, szablon)
-                  
-        return out
-        
+            inputs = liczba[0:ileNaRaz]
+            stan = liczba[ileNaRaz:]
+            for bitStanu in reversed(liczba):
+                b = 0
+                if(bitStanu == '1'):
+                    b=1
+                self.__rej.shift(b)
+
+            stanNaWyjsciu = str(self.__rej)[ileNaRaz:]
+            outcome = list(self.__rej.licz())
+
+            daneStanu = {'outputBits':outcome, 'destState': stanNaWyjsciu}
+            self.__stany[stan] = daneStanu
+        print(self.__stany)
+        print()
     # zwraca slownik
     # [in] = inptBits
     # [out] = output bits
