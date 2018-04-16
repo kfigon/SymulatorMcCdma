d6bb0c421f0bc49fa918695d60971d1183d19b93
kfigon <kamil.figon@jitsolutions.pl>
2018-04-16 15:39:17 +0200

    modulator dziala

--- modulatorQpsk.py
+++ modulatorQpsk.py
@@ -2,32 +2,25 @@ import math
 import utils
 
 class Qpsk:
-    def __init__(self, fp, fn, ampl=1):
+    def __init__(self, fp, fn, fb, ampl=1):
         '''
         :param fp: cz. probkowania
         :param fn: cz. nosnej
+        :param fb: cz. bitow (2 razy wieksza niz symboli)
         :param ampl:
         '''
         self.__fp = fp
         self.__fn = fn
+        self.__fb = fb
         self.__ampl = ampl
 
     def generujCzas(self, czasTrwania):
-        for i in range(0, czasTrwania * self.__fp):
+        for i in range(czasTrwania * self.__fp):
             yield (i / self.__fp)
-
-    def generujNosna(self, czas, faza=0):
-        '''
-        :param czas: wynik z generatora czasu
-        :param faza: faza cosinusa w radianach
-        :return:
-        '''
-        for t in czas:
-            yield self.__ampl * math.cos(2 * math.pi * self.__fn * t + faza)
 
     def __getQI(self, dane, czyI):
         if(len(dane) % 2 != 0):
-            raise Exception('Dlugosc powinna byc parzysta')
+            raise Exception('Dlugosc danych powinna byc parzysta, jest {}'.format(str(len(dane))))
 
         modulo = 1
         if(czyI):
@@ -43,17 +36,48 @@ class Qpsk:
         return self.__getQI(dane, True)
 
     def moduluj(self, dane):
-        pass
+        if(len(dane) % 2 != 0):
+            raise Exception('Dlugosc danych powinna byc parzysta, jest {}'.format(str(len(dane))))
+        
+        czasTrwania = self.getCzasTransmisji(len(dane), self.__fb) 
+        czas = self.generujCzas(czasTrwania)
+
+        qbity = self.getQ(dane)
+        ibity = self.getI(dane)
+
+        probkiQ = utils.probkujGen(qbity, self.__fp, self.__fb/2)
+        probkiI = utils.probkujGen(ibity, self.__fp, self.__fb/2)
+        
+        out = []
+        for t, q, i in zip(czas, probkiQ, probkiI):
+            faza = self.__symbol2Faza(i,q)
+            x = self.__ampl*math.cos(faza)*math.cos(2*math.pi*self.__fn*t)
+            y = self.__ampl*math.sin(faza)*math.sin(2*math.pi*self.__fn*t)
+            out.append(x-y)
+        return out
+
+    def walidujDlugosci(self, dane):
+        qbity = self.getQ(dane)
+        ibity = self.getI(dane)
+        probkiQ = list(utils.probkujGen(qbity, self.__fp, self.__fb/2))
+        probkiI = list(utils.probkujGen(ibity, self.__fp, self.__fb/2))
+        czasTrwania = self.getCzasTransmisji(len(dane), self.__fb) 
+        czas = list(self.generujCzas(czasTrwania))        
+
+        dlCzas = len(czas)
+        dI = len(probkiI)
+        dQ = len(probkiQ)
+        if(dlCzas != dI and dlCzas != dQ and dI != dQ):
+            raise Exception('dlugosc czasu: {}, dlugosc I {}, dlugosc Q {}'.format(
+                        str(dlCzas), 
+                        str(dI),
+                        str(dQ)))
 
     def getCzasTransmisji(self, ileBitow, fb):
         return int(ileBitow/fb)
 
-    def mapBipolar(self, bit):
-        if(bit == 0):
-            return 1
-        return -1
 
-    def symbol2Faza(self, i, q):
+    def __symbol2Faza(self, i, q):
         if(i == 0 and q == 0):
             return math.pi/4
         elif(i == 0 and q == 1):
@@ -62,38 +86,25 @@ class Qpsk:
             return 5*math.pi / 4
         elif (i == 1 and q == 1):
             return 7*math.pi / 4
-        raise Exception('Blad I{}, Q{}'.format(str(i), str(q)))
+        raise Exception('Blad, nie ma fazy dla: I{}, Q{}'.format(str(i), str(q)))
 
-import matplotlib.pyplot as plt
-def main():
-    dane = utils.generujDaneBinarneGen(10)
-    fp = 10
-    fn=2
-    fb = 1
-    probki = utils.probkuj(dane, fp,fn)
-    qpsk = Qpsk(fp, fn)
 
-    q = qpsk.getQ(dane)
-    i = qpsk.getI(dane)
 
-    modulowany = []
-    czas
-    while(True):
-        try:
-            qi = next(q)
-            ii = next(i)
-            faza = qpsk.symbol2Faza(ii, qi)
-            nosnaSymbolu = qpsk.generujNosna()
-        except:
-            break
+# todo: odkomentowac dla testu
 
-    #  todo: przejscie na kanaly IQ
-    # 'spowalnia' bitrate o 2.
-    #  dane 10Hz, I 5, Q 5
-    plt.subplot(2,1,1)
-    plt.stem(dane)
-    plt.subplot(2, 1, 2)
-    plt.stem(modulowany)
-    plt.show()
+# import matplotlib.pyplot as plt
+# def wykresQpsk():
+#     dane = utils.generujDaneBinarne(20)
+#     fp = 50
+#     fn=2
+#     fb = 1
+#     qpsk = Qpsk(fp, fn,fb)
+#     modulowany = qpsk.moduluj(dane)
 
-main()
+#     plt.subplot(2,1,1)
+#     plt.stem(dane)
+#     plt.subplot(2, 1, 2)
+#     plt.plot(modulowany)
+#     plt.show()
+
+# wykresQps()
--- testy/testyModulatoraQpsk.py
+++ testy/testyModulatoraQpsk.py
@@ -1,20 +1,10 @@
 import unittest
 from modulatorQpsk import Qpsk
+import utils
 
 class TestModulatoraQpsk(unittest.TestCase):
     def setUp(self):
-        self.q = Qpsk(12, 1)
-
-    def testGenerowaniaNosnej(self):
-        exp = [1.0, 0.8660254037844387, 0.5000000000000001, 6.123233995736766e-17, -0.4999999999999998, -0.8660254037844387,
-         -1.0, -0.8660254037844386, -0.5000000000000004, -1.8369701987210297e-16, 0.5000000000000001,
-         0.8660254037844384, 1.0, 0.866025403784439, 0.4999999999999997, 3.061616997868383e-16, -0.4999999999999992,
-         -0.8660254037844388, -1.0, -0.8660254037844392, -0.49999999999999983, -4.286263797015736e-16,
-         0.4999999999999991, 0.8660254037844387, 1.0, 0.8660254037844383, 0.5000000000000014, 5.51091059616309e-16,
-         -0.5000000000000006, -0.8660254037844377, -1.0, -0.8660254037844393, -0.5000000000000016,
-         -2.4499125789312946e-15, 0.5000000000000004, 0.8660254037844386]
-        czas = self.q.generujCzas(3)
-        self.assertAlmostEqual(exp, list(self.q.generujNosna(czas)))
+        self.q = Qpsk(10, 2,1)
 
     def testSzeregowoRownoleglego(self):
         dane = [1,0,1,0,0,1]
@@ -26,10 +16,13 @@ class TestModulatoraQpsk(unittest.TestCase):
         self.assertEqual(10, self.q.getCzasTransmisji(5, 0.5))
         self.assertEqual(5, self.q.getCzasTransmisji(10, 2))
 
+    def testWalidacjiDlugosci(self):
+        dane = utils.generujDaneBinarne(10)
+        self.q.walidujDlugosci(dane)
+
     def testMod(self):
-        dane = [1,0,1,0,0]
-        self.fail('todo')
-
-
+        zmodulowany = self.q.moduluj(utils.generujDaneBinarne(10))
+        self.assertEqual(100, len(zmodulowany))
+        
 if __name__ == '__main__':
     unittest.main()
