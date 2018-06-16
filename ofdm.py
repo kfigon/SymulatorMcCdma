9ce0a0d82bb161c3544f2773f0e3c9305672d97b
kfigon <kamil.figon@jitsolutions.pl>
2018-06-16 13:36:35 +0200

    ofdm dziala?

--- ofdm.py
+++ ofdm.py
@@ -1,66 +1,60 @@
 import scipy.fftpack as fft
-from modulatorQpsk import Qpsk
 import utils
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from przetwornikSP import PrzetwornikSzeregowoRownolegly
+from modulatorQpsk import Qpsk
 
-class Ofdm(Qpsk):
-    def __init__(self, fp, fn, fb, ampl=1):
-        super().__init__(fp, fn, fb, ampl)
+class Ofdm:
+    def __init__(self, fp, fb, ileStrumieni):
         self.__fp = fp
-        self.__fn = fn
         self.__fb=fb
-        self.__ampl = ampl
+        self.__ileStrumieni = ileStrumieni
 
-    def __bity2Symbole(self, bity):
-        for i in range(len(bity)-1):
-            yield self.__mapujNaSymbol(bity[i], bity[i+1])
+    def __wyliczCzestotliwosci(self):
+        out=[]
+        for i in range(self.__ileStrumieni):
+            out.append((i+1)*10)
+        return out
 
-    def __mapujNaSymbol(self, ib, qb):
-        if ib == 0 and qb == 0:
-            return complex(1,1)
-        elif ib==0 and qb == 1:
-            return complex(-1,1)
-        elif ib==1 and qb==0:
-            return complex(-1,-1)
-        else:
-            return complex(1, -1)
 
     def moduluj(self, dane):
         if(len(dane) % 2 != 0):
             raise Exception('Dlugosc danych powinna byc parzysta, jest {}'.format(str(len(dane))))
-        
-        symbole = self.__bity2Symbole(dane)
-        probkiSymboli = list(utils.probkujGen(symbole, self.__fp, self.__fb/2))
-        
-        transformata = fft.ifft(probkiSymboli)
+    
 
-        czasTrwania = utils.getCzasTransmisji(len(dane), self.__fb) 
-        czas = self.generujCzas(czasTrwania)
+        przetwornik = PrzetwornikSzeregowoRownolegly(self.__ileStrumieni)
+        strumienie = przetwornik.rozdziel(dane)
+        czestotliwosci = self.__wyliczCzestotliwosci()
 
         out = []
-        for t, f in zip(czas, transformata):
+
+        for s,c in zip(strumienie, czestotliwosci):
+            q = Qpsk(self.__fp, c, self.__fb//self.__ileStrumieni)
+            wynik = q.moduluj(s)
             
-            x = self.__ampl*f.real*math.cos(2*math.pi*self.__fn*t)
-            y = self.__ampl*f.imag*math.sin(2*math.pi*self.__fn*t)
-            out.append(complex(x,y))
+            if(len(out) == 0):
+                for b in wynik:
+                    out.append(b)
+            else:
+                for i in range(len(out)):
+                    out[i] += wynik[i]
         return out        
 
 def main():
     dane = utils.generujDaneBinarne(30)
-    fp = 100
-    fn= 3
+    fp = 200
     fb =5
-    ofdm  = Ofdm(fp, fn,fb)
+    ofdm  = Ofdm(fp,fb, 3)
     modulowany = ofdm.moduluj(dane)
-    N = len(modulowany)
 
     plt.subplot(2,1,1)
-    plt.plot(list(map(lambda x: abs(x), modulowany)))
+    plt.plot(modulowany)
 
     plt.subplot(2,1,2)
-    plt.stem(abs(fft.fft(modulowany[0:int(N/2)])))
+    plt.plot(abs(fft.fft(modulowany)))
+    
     plt.show()
 
 if __name__ == '__main__':
