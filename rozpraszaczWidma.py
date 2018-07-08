b217d0fd3862e6b431853a0cc3481aace26ec91c
kfigon <kamil.figon@jitsolutions.pl>
2018-07-08 11:36:25 +0200

    rozpraszacz poprawiony

--- rozpraszaczWidma.py
+++ rozpraszaczWidma.py
@@ -2,16 +2,12 @@ class RozpraszaczWidma:
     def __init__(self, ileChipowNaBit):
         self.__ileChipowNaBit = ileChipowNaBit
     
-    def __rozpraszaj(self,dane, ciag):
+    def rozpraszaj(self, dane, ciagRozpraszajacy):
         i = 0
         for d in dane:
             for _ in range(self.__ileChipowNaBit):
-                yield d ^ ciag[i]
-                i +=1
-
-    def rozpraszaj(self, dane, ciagRozpraszajacy):
-        if (len(dane)*self.__ileChipowNaBit) != len(ciagRozpraszajacy):
-            raise Exception('Nieprawidlowe dlugosci ciagow. Dane {}, ciag {}, ileChipowNaBit {}'.format(str(len(dane)), str(len(ciagRozpraszajacy)), str(self.__ileChipowNaBit)))
-
-        return self.__rozpraszaj(dane, ciagRozpraszajacy)
+                yield d ^ ciagRozpraszajacy[i]
+                i += 1
+                if i >= len(ciagRozpraszajacy):
+                    i = 0
     
--- test/testRozpraszacza.py
+++ test/testRozpraszacza.py
@@ -3,19 +3,33 @@ from rozpraszaczWidma import RozpraszaczWidma
 
 
 class TestRozpraszacza(unittest.TestCase):
-    def test1(self):
+    def testOk(self):
         r = RozpraszaczWidma(3) #3 chipy na bit
         bity = [1,0,0,1]
         ciagRozpraszajacy = [1,1,1,0,0,1,0,0,1,0,0,0]
         exp=[0,0,0, 0,0,1, 0,0,1, 1,1,1]
         self.assertEqual(exp, list(r.rozpraszaj(bity, ciagRozpraszajacy)))
 
-    def test2(self):
-        r = RozpraszaczWidma(3) #3 chipy na bit
-        bity = [1,0,0,1]
+    def testKrotszeDane(self):
+        r = RozpraszaczWidma(3)
+        bity = [1,0]
         ciagRozpraszajacy = [1,1,1,0,0,1,0,0,1,0,0]
-        with self.assertRaises(Exception):
-            r.rozpraszaj(bity,ciagRozpraszajacy)
+        exp = [0,0,0, 0,0,1]
+        self.assertEqual(exp, list(r.rozpraszaj(bity, ciagRozpraszajacy)))
+
+    def testDluzszeDane(self):
+        r = RozpraszaczWidma(3)
+        bity = [1,0,1]
+        ciagRozpraszajacy = [1,1,1,0,0,1]
+        exp = [0,0,0, 0,0,1, 0,0,0]
+        self.assertEqual(exp, list(r.rozpraszaj(bity, ciagRozpraszajacy)))
+    
+    def testNotAligned(self):
+        r = RozpraszaczWidma(3)
+        bity = [1,0,1]
+        ciagRozpraszajacy = [1,1,1,0,0,1,0]
+        exp = [0,0,0, 0,0,1, 1,0,0]
+        self.assertEqual(exp, list(r.rozpraszaj(bity, ciagRozpraszajacy)))
 
 if __name__ == '__main__':
     unittest.main()
