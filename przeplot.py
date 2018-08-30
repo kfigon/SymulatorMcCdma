3bef61df168c7f000c50526a163ed19fb923b745
kfigon <kamil.figon@jitsolutions.pl>
2018-08-30 20:55:23 +0200

    przeplot done

--- przeplot.py
+++ przeplot.py
@@ -1,20 +1,28 @@
 import math
 
 class Przeplatacz:
-    def __init__(self, iloscKolumn):
+    def __init__(self, szerokoscPrzeplotu):
         ''' przeplot realizowany na zasadzie transpozycji macierzy '''
-        self.__iloscKolumn = iloscKolumn
+        self.szerokoscPrzeplotu = szerokoscPrzeplotu
 
     def przeplot(self, dane):
+        if len(dane) % self.szerokoscPrzeplotu != 0:
+            raise Exception('Ilosc danych {} powinna byc podzielna przez krok przeplotu {}'
+            .format(str(len(dane),str(self.szerokoscPrzeplotu))))
+
+        return self._przeplotInternal(dane, self.szerokoscPrzeplotu)
+
+    def _przeplotInternal(self, dane, krok):
         out = []
         dl = len(dane)
-        for i in range(self.__iloscKolumn):
+        for i in range(krok):
             j = i
             while(j < dl):
                 out.append(dane[j])
-                j += self.__iloscKolumn
+                j += krok
 
         return out
 
     def rozplot(self, dane):
-        return self.przeplot(dane)
+        krok = math.ceil(len(dane)/self.szerokoscPrzeplotu)
+        return self._przeplotInternal(dane, krok)
--- test/testPrzeplotu.py
+++ test/testPrzeplotu.py
@@ -3,29 +3,24 @@ from przeplot import Przeplatacz
 
 class TestPrzeplotu(unittest.TestCase):
 
-    def test(self):
+    def testAligned(self):
         dane = [
             {'in':[1,2,3, 4,5,6, 7,8,9], 'exp':[1,4,7, 2,5,8, 3,6,9], 'szerokoscPrzeplotu': 3},
-            {'in':[1,2,3, 4,5,6, 7,8], 'exp':[1,4,7, 2,5,8, 3,6], 'szerokoscPrzeplotu': 3},
             {'in':[1,2,3,4 ,5,6,7,8], 'exp':[1,5, 2,6, 3,7, 4,8], 'szerokoscPrzeplotu': 4},
             {'in':[1,2,3,4,5 ,6,7,8,9,10, 11,12,13,14,15], 
                 'exp':[1,6,11, 2,7,12, 3,8,13, 4,9,14, 5,10,15], 'szerokoscPrzeplotu': 5},
-
-            {'in':[1,2,3,4,5 ,6,7,8,9,10, 11,12,13], 
-                'exp':[1,6,11, 2,7,12, 3,8,13, 4,9, 5,10], 'szerokoscPrzeplotu': 5}
             ]
 
-        for i,d in enumerate(dane):
-            nazwa = 'test '+str(i)+'. z szerokoscia ' + str(d['szerokoscPrzeplotu'])
+        for d in dane:
+            nazwa = 'test z szerokoscia ' + str(d['szerokoscPrzeplotu'])
             p = Przeplatacz(d['szerokoscPrzeplotu'])
-            
+                
             with self.subTest(name=nazwa):
                 przeplecione = p.przeplot(d['in'])
-                self.assertEqual(d['exp'], przeplecione)
+                self.assertEqual(d['exp'], przeplecione, 'przeplot failed')
 
                 rozplecione = p.rozplot(przeplecione)
-                self.assertEqual(d['in'], rozplecione)
-
+                self.assertEqual(d['in'], rozplecione, 'rozplot failed')
         
 
 if __name__ == '__main__':
