04da8f186b8e7b13b464ae36ad2dadd66a682096
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-09-01 15:07:23 +0200

    refactor kodera

--- koderSplotowy.py
+++ koderSplotowy.py
@@ -5,21 +5,23 @@ from viterbi import Viterbi
 
 class KoderSplotowy:
     def __init__(self, rejestrPrzesuwny, ileBitowNaRaz = 1):
-        self.__rejestr = rejestrPrzesuwny
         self.__ileBitowNaRaz = ileBitowNaRaz
+        self.__maszyna = MaszynaStanow(rejestrPrzesuwny, self.__ileBitowNaRaz)
+
+        self.__nkm = (
+                rejestrPrzesuwny.getIleBitowWyjsciowych(),
+                self.__ileBitowNaRaz, 
+                rejestrPrzesuwny.getDlugoscRejestru())
 
     def kodujBezMultipleksowania(self, daneBinarne):
         assert(len(daneBinarne) % self.__ileBitowNaRaz == 0)
-
-        self.reset()
-        maszyna = MaszynaStanow(self.__rejestr, self.__ileBitowNaRaz)
         
         krok = self.__ileBitowNaRaz
-        stan = maszyna.getStanPoczatkowy()
+        stan = self.__maszyna.getStanPoczatkowy()
 
         for i in range(0, len(daneBinarne), krok):
             podCiag = daneBinarne[i:i+krok]
-            daneStanu = maszyna.checkState(stan, podCiag)
+            daneStanu = self.__maszyna.checkState(stan, podCiag)
             stan = daneStanu['outState']
             yield daneStanu['out']
 
@@ -29,20 +31,14 @@ class KoderSplotowy:
         return flat(out)
 
     def getNKM(self):
-        return (self.__rejestr.getIleBitowWyjsciowych(),
-                self.__ileBitowNaRaz, self.__rejestr.getDlugoscRejestru())
-
-    def reset(self):
-        self.__rejestr.reset()
+        return self.__nkm
 
     def dekoduj(self, daneBinarne):
-        self.reset()
-        maszyna = MaszynaStanow(self.__rejestr, self.__ileBitowNaRaz)
-        v = Viterbi(maszyna)
+        v = Viterbi(self.__maszyna)
 
         # todo: potencjalne miejsce do optymalizacji, 
         # nie tworzyc nowej tablicy, uzyc indeksow
-        podzielone = podziel(daneBinarne, self.__rejestr.getIleBitowWyjsciowych())
+        podzielone = podziel(daneBinarne, self.getNKM()[0])
 
         for i, paczka in enumerate(podzielone):
             if(i==0):
