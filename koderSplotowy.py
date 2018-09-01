f75037b71044fc412c78f53538f0e5d4e3f2eb8f
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-09-01 14:18:52 +0200

    kodowanie odzielone od multipleksowania

--- koderSplotowy.py
+++ koderSplotowy.py
@@ -1,6 +1,6 @@
 from rejestrPrzesuwny import RejestrPrzesuwny
 from maszynaStanow import MaszynaStanow
-from utils import podziel
+from utils import podziel, flat
 from viterbi import Viterbi
 
 class KoderSplotowy:
@@ -8,28 +8,29 @@ class KoderSplotowy:
         self.__rejestr = rejestrPrzesuwny
         self.__ileBitowNaRaz = ileBitowNaRaz
 
-    def koduj(self, daneBinarne):
+    def kodujBezMultipleksowania(self, daneBinarne):
         assert(len(daneBinarne) % self.__ileBitowNaRaz == 0)
         self.reset()
 
-        dl = self.__rejestr.getIleBitowWyjsciowych()*len(daneBinarne)//self.__ileBitowNaRaz
-        out = [0]*dl
         krok = self.__ileBitowNaRaz
-        idxOut=0
 
         for i in range(0, len(daneBinarne), krok):
             podCiag = daneBinarne[i:i+krok]
+
             # wchodza wszystkie na raz!
             for obrot in reversed(range(krok)):
                 b = podCiag[obrot]
                 self.__rejestr.shift(b)
             wynikObrotu = self.__rejestr.licz()
+            
+            # tablica tablic (kolejne wyjscia z poszczegolnych odczepow)
+            yield wynikObrotu
 
-            # multipleksowanie (p/s)
-            for b in wynikObrotu:
-                out[idxOut] = b
-                idxOut += 1
-        return out
+
+    def koduj(self, daneBinarne):
+        out = self.kodujBezMultipleksowania(daneBinarne)
+        # multipleksowanie (p/s)
+        return flat(out)
 
     def getNKM(self):
         return (self.__rejestr.getIleBitowWyjsciowych(),
