186fc2d42fa0b211d26052791743014497d924cd
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-09-06 21:11:56 +0200

    beta i alfa fun added

--- map.py
+++ map.py
@@ -5,7 +5,7 @@ def liczL(prawodobienstwo1, prawodopobienstwo0):
 
 def decyzja(val):
     return 1 if val >=0 else -1
-    
+
 def mapujBit(val):
     return -1 if val == 0 else 1
 
@@ -18,22 +18,28 @@ class MapAlgorithm:
         self.__ck = Ck
         self.__lc=Lc
 
-    def gamma(self, stan, dane, parzystosc, prawdopodo0=1/2, prawdopodo1=1/2):
+    def gamma(self, stanPocz, stanKoncowy, dane, parzystosc, prawdopodo0=1/2, prawdopodo1=1/2):
         '''
         pojedyczna iteracja prawodobienstwa
         przejscia ze stanu s` do s
+        s` = parametr stan
+        zwraca gamma dla 0 i dla 1
         '''
-        outPrzy0Bin = self.__maszyna.checkState(stan, [0])['out']
-        outPrzy1Bin = self.__maszyna.checkState(stan, [1])['out']
-
-        outPrzy0 = list(map(mapujBit, outPrzy0Bin))
-        outPrzy1 = list(map(mapujBit, outPrzy1Bin))
+        przejscie = self.__maszyna.getStan(stanPocz, stanKoncowy)
+        out = mapujBit(przejscie['in'][0])
         
-        a = self.__liczGammaInternal(prawdopodo0, prawdopodo1, dane, parzystosc, outPrzy0)
-        b = self.__liczGammaInternal(prawdopodo0, prawdopodo1, dane, parzystosc, outPrzy1)
-        return (a,b)
+        g = self.__liczGammaInternal(prawdopodo0, prawdopodo1, dane, parzystosc, out)
+        return g
 
     def __liczGammaInternal(self, prawdopodo0, prawdopodo1, dane, parzystosc, out):
-        sumuj = lambda d, p, out:  d*out[0]+p*out[1]
+        sumuj = lambda d, p, out:  d*out + p*out
         uk = 1
         return self.__ck*uk*math.exp(liczL(prawdopodo0,prawdopodo1)/2)*math.exp(self.__lc/2 *sumuj(dane,parzystosc, out))
+
+    def alfa(self, alfa1, gamma1, alfa2, gamma2):
+        '''poprzednie alfa i przejscie z niego do obecnego stanu
+        dla obu sciezek'''
+        return  alfa1*gamma1 + alfa2*gamma2
+
+    def beta(self, beta1, gamma1, beta2, gamma2):
+        return beta1*gamma1 + beta2*gamma2
--- test/testMap.py
+++ test/testMap.py
@@ -19,14 +19,40 @@ class Test(unittest.TestCase):
         self.assertEqual(-1, decyzja(-1))
         self.assertEqual(1, decyzja(12))
 
-    def testGamma(self):
+    def testGamma0(self):
         stan='00'
+        stanKoncowy = '00'
         odebranaDana=0.3
         odebranaParzystosc=0.1
 
-        g = self.m.gamma(stan, odebranaDana, odebranaParzystosc)
-        self.assertAlmostEqual(0.368, g[0], 3)
-        self.assertAlmostEqual(2.718, g[1], 3)
-        
+        g = self.m.gamma(stan, stanKoncowy, odebranaDana, odebranaParzystosc)
+        self.assertAlmostEqual(0.368, g, 3)
+    
+    def testGamma1(self):
+        stan='00'
+        stanKoncowy = '10'
+        odebranaDana=0.3
+        odebranaParzystosc=0.1
+
+        g = self.m.gamma(stan, stanKoncowy, odebranaDana, odebranaParzystosc)
+        self.assertAlmostEqual(2.718, g, 3)
+
+    def testAlfa(self):
+        gamma1 = 0.5
+        poprzednieAlfa1 = 0.4
+        gamma2 = 0.1
+        poprzednieAlfa2 = 0.2
+
+        exp = 0.22
+        self.assertAlmostEqual(exp, self.m.alfa(poprzednieAlfa1, gamma1, poprzednieAlfa2, gamma2))
+    def testBeta(self):
+        gamma1 = 0.5
+        beta1 = 0.4
+        gamma2 = 0.1
+        beta2 = 0.2
+
+        exp = 0.22
+        self.assertAlmostEqual(exp, self.m.beta(beta1, gamma1, beta2, gamma2))
+
 if __name__ == '__main__':
     unittest.main()
