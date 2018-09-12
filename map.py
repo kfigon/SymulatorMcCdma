a452f37a9c28197eedeb5bca3f987e70df99f241
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-09-12 18:35:41 +0200

    calculations pending

--- map.py
+++ map.py
@@ -9,94 +9,98 @@ def decyzja(val):
 def mapujBit(val):
     return -1 if val == 0 else 1
 
+def gamma(odebrane, zakodowane, uk, Luk, Lc):
+    iloczyny = [x*y for x,y in zip(odebrane, zakodowane)]
+    suma = sum(iloczyny)
+    return math.exp(uk * Luk/2)*math.exp(Lc*suma/2)
+
+def alfa(a, g):
+    '''poprzednie alfa i korespadujace z nim gamma'''
+    return sum([x*y for x,y in zip(a, g)])
+
+def normujAlfa(alfa, wszystkieAlfa):
+    return alfa/sum(wszystkieAlfa)
+
+
 class MapAlgorithm:
-    def __init__(self, maszyna, Ck=1, Lc=5):
+    def __init__(self, maszyna, uk=0, Luk=0, Lc=5):
         '''
         algorytm MAP
         '''
         self.__maszyna = maszyna
-        self.__ck = Ck
-        self.__lc=Lc
+        self.__uk=uk # ?
+        self.__luk=Luk # prawdopodobienstwo zajscia uk
+        self.__lc=Lc # miara jakosci kanalu
 
-    def gamma(self, stanPocz, stanKoncowy, dane, parzystosc, prawdopodo0=1/2, prawdopodo1=1/2):
-        '''
-        pojedyczna iteracja prawodobienstwa
-        przejscia ze stanu s` do s
-        s` = parametr stan
-        zwraca gamma dla 0 i dla 1
-        '''
-        przejscie = self.__maszyna.getStan(stanPocz, stanKoncowy)
-        out = mapujBit(przejscie['in'][0])
+    def licz(self, odebrane, apriori=[]):
+        '''liczy iteracje map
+        odebrane w formie 2 wymiarowej tablicy - [ [symbol1], [symbol2] ]'''
         
-        g = self.__liczGammaInternal(prawdopodo0, prawdopodo1, dane, parzystosc, out)
-        return g
+        # wyliczyc wszystkie gamma
+        # wyliczyc wszystkie alfa (+ normowanie)
+        # beta
+        # wyliczyc L, decyzja przy ostatniej iteracji.
+        if len(apriori) = 0:
+            pass # todo: zainicjalizowac zerami jesli nie podano. jesli podano to wyrzkosyatc
 
-    def __liczGammaInternal(self, prawdopodo0, prawdopodo1, dane, parzystosc, out):
-        sumuj = lambda d, p, out:  d*out + p*out
-        uk = 1
-        return self.__ck*uk*math.exp(liczL(prawdopodo0,prawdopodo1)/2)*math.exp(self.__lc/2 *sumuj(dane,parzystosc, out))
+        ileStanow = self.__maszyna.getNumberOfStates()
+        ileOdebranychSymboli = len(odebrane)
+        
+        alfy = [[0 for _ in range(ileStanow)] for _ in range(ileOdebranychSymboli)]
+        bety = [[0 for _ in range(ileStanow)] for _ in range(ileOdebranychSymboli)]
+        gammy = [ [[0 for _ in range(ileStanow)] for _ in range(ileStanow)] for _ in range(ileOdebranychSymboli) ]
 
-    def alfa(self, alfa1, gamma1, alfa2, gamma2):
-        '''poprzednie alfa i przejscie z niego do obecnego stanu
-        dla obu sciezek'''
-        return  alfa1*gamma1 + alfa2*gamma2
+        stany = self.__maszyna.getListaStanow()        
 
-    def beta(self, beta1, gamma1, beta2, gamma2):
-        return beta1*gamma1 + beta2*gamma2
+        # pewne stany
+        alfy[0][0] = 1
+        bety[ileOdebranychSymboli-1][0]=1
 
-import math
 
-if __name__ =="__main__":
-    def gamma(odebrane, zakodowane, uk=0, Luk=0, Lc=5):
-        sumy = [x*y for x,y in zip(odebrane, zakodowane)]
-        suma = sum(sumy)
-        return math.exp(uk * Luk/2)*math.exp(Lc*suma/2)
 
-    def alfa(a, g):
-        '''poprzednie alfa i korespadujace z nim gamma'''
-        return sum([x*y for x,y in zip(a, g)])
-    def normuj(alfa, wszystkieAlfa):
-        return alfa/sum(wszystkieAlfa)
+        return [1,2,3]
 
-    mapping = {
-        '00':   [-1,-1],
-        '02':   [1,1],
-        '10':   [1,1],
-        '12':   [-1,-1],
-        '21':   [1,-1],
-        '23':   [-1,1],
-        '31':   [-1,1],
-        '33':   [1,-1],
-    }
+# if __name__ =="__main__":
 
-    odebrane = [[0.3, 0.1], [-0.5, 0.2], [0.8, 0.5], [-0.5, 0.3]]
-    Lc = 5
-    Luk = 0 # prawdopodobienstwo Uk=1 -> 1/2
-    uk = 123 # any, since Luk is 0
-    g = [gamma(odebrane[0], mapping['00']), gamma(odebrane[0], mapping['02'])]
-    print(g)
+    # mapping = {
+    #     '00':   [-1,-1],
+    #     '02':   [1,1],
+    #     '10':   [1,1],
+    #     '12':   [-1,-1],
+    #     '21':   [1,-1],
+    #     '23':   [-1,1],
+    #     '31':   [-1,1],
+    #     '33':   [1,-1],
+    # }
 
-    a00n=alfa([1],[g[0]])
-    a02n=alfa([1],[g[1]])
+    # odebrane = [[0.3, 0.1], [-0.5, 0.2], [0.8, 0.5], [-0.5, 0.3]]
+    # Lc = 5
+    # Luk = 0 # prawdopodobienstwo Uk=1 -> 1/2
+    # uk = 123 # any, since Luk is 0
+    # g = [gamma(odebrane[0], mapping['00']), gamma(odebrane[0], mapping['02'])]
+    # print(g)
+
+    # a00n=alfa([1],[g[0]])
+    # a02n=alfa([1],[g[1]])
     
-    a00 = normuj(a00n, [a00n, a02n])
-    a02 = normuj(a02n,[a00n, a02n])
+    # a00 = normuj(a00n, [a00n, a02n])
+    # a02 = normuj(a02n,[a00n, a02n])
 
-    print(a00, a02)
+    # print(a00, a02)
 
-    g2 = [
-        gamma(odebrane[1], mapping['00']),
-        gamma(odebrane[1], mapping['02']),
-        gamma(odebrane[1], mapping['21']),
-        gamma(odebrane[1], mapping['23']),
-        ]
-    print(g2)
+    # g2 = [
+    #     gamma(odebrane[1], mapping['00']),
+    #     gamma(odebrane[1], mapping['02']),
+    #     gamma(odebrane[1], mapping['21']),
+    #     gamma(odebrane[1], mapping['23']),
+    #     ]
+    # print(g2)
 
-    a2n = [
-        alfa([a00],[g2[0]]),
-        alfa([a00],[g2[1]]),
-        alfa([a02],[g2[2]]),
-        alfa([a02],[g2[3]]),
-    ]
-    a2 = [normuj(i,a2n) for i in a2n]
-    print(a2)
+    # a2n = [
+    #     alfa([a00],[g2[0]]),
+    #     alfa([a00],[g2[1]]),
+    #     alfa([a02],[g2[2]]),
+    #     alfa([a02],[g2[3]]),
+    # ]
+    # a2 = [normuj(i,a2n) for i in a2n]
+    # print(a2)
--- test/testMap.py
+++ test/testMap.py
@@ -1,15 +1,9 @@
 import unittest
-from map import MapAlgorithm, liczL, decyzja
+from map import MapAlgorithm, liczL, decyzja, gamma, alfa, normujAlfa
 from maszynaStanow import MaszynaStanow
 from rejestrPrzesuwny import RejestrPrzesuwny
 
-class Test(unittest.TestCase):
-    def setUp(self):
-        odczepy = [[0,1,2],[0,2]]
-        rej = RejestrPrzesuwny(3, odczepy)
-        maszyna = MaszynaStanow(rej, 1)
-        self.m = MapAlgorithm(maszyna)
-
+class TestMatematykiMap(unittest.TestCase):
     def testL(self):
         x = liczL(1/2, 1/2)
         self.assertEqual(0, x)
@@ -19,40 +13,38 @@ class Test(unittest.TestCase):
         self.assertEqual(-1, decyzja(-1))
         self.assertEqual(1, decyzja(12))
 
-    def testGamma0(self):
-        stan='00'
-        stanKoncowy = '00'
-        odebranaDana=0.3
-        odebranaParzystosc=0.1
+    def testGamma(self):
+        odebrane=[0.3, 0.1]
+        Lc = 5
+        Luk = 0
+        uk = 123 # any, since Luk=0
+        zakodowane = [-1,-1]
+        g = gamma(odebrane, zakodowane, uk, Luk, Lc)
 
-        g = self.m.gamma(stan, stanKoncowy, odebranaDana, odebranaParzystosc)
-        self.assertAlmostEqual(0.368, g, 3)
-    
-    def testGamma1(self):
-        stan='00'
-        stanKoncowy = '10'
-        odebranaDana=0.3
-        odebranaParzystosc=0.1
-
-        g = self.m.gamma(stan, stanKoncowy, odebranaDana, odebranaParzystosc)
-        self.assertAlmostEqual(2.718, g, 3)
+        self.assertAlmostEqual(0.3678, g, 3)
 
     def testAlfa(self):
-        gamma1 = 0.5
-        poprzednieAlfa1 = 0.4
-        gamma2 = 0.1
-        poprzednieAlfa2 = 0.2
+        a=[1,2,3,4]
+        g=[2,2,3,5]
+        wynik = alfa(a,g)
+        self.assertEqual(35, wynik)
+    
+    def testNormowaniaAlfa(self):
+        a=50
+        alfy=[1,2,3,4]
+        self.assertEqual(5.0, normujAlfa(a, alfy))
 
-        exp = 0.22
-        self.assertAlmostEqual(exp, self.m.alfa(poprzednieAlfa1, gamma1, poprzednieAlfa2, gamma2))
-    def testBeta(self):
-        gamma1 = 0.5
-        beta1 = 0.4
-        gamma2 = 0.1
-        beta2 = 0.2
+class TestMap(unittest.TestCase):
+    def setUp(self):
+        odczepy = [[0,1,2],[0,2]]
+        rej = RejestrPrzesuwny(3, odczepy)
+        maszyna = MaszynaStanow(rej, 1)
+        self.m = MapAlgorithm(maszyna)
 
-        exp = 0.22
-        self.assertAlmostEqual(exp, self.m.beta(beta1, gamma1, beta2, gamma2))
-
+    def test(self):
+        odebrane = [[0.3,0.1],[-0.5,0.2],[0.8,0.5],[-0.5,0.3],[0.1,-0.7],[1.5,-0.4]]
+        wynik = self.m.licz(odebrane)
+        self.assertEqual([], wynik)
+        
 if __name__ == '__main__':
     unittest.main()
