c000d5ac5d19c52a3cef76533a1f01e96b74169c
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-09-18 18:41:21 +0200

    ofdm zrobiony

--- learningOfdm/ofdm.py
+++ learningOfdm/ofdm.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+# ofdm + qpsk
+
 def generujDaneBinarne(ile):
     out = []
+    ile//2
+    bipolar = lambda x: 1 if x>=0.5 else -1
     for _ in range(ile):
-        bit = 1 if random.random() >=0.5 else -1
-        out.append(bit) 
+        bitI = bipolar(random.random())
+        bitQ = bipolar(random.random())
+        out.append(complex(bitI, bitQ)) 
     return out
 
 def moduluj(bity):
@@ -17,12 +22,13 @@ def moduluj(bity):
 def addPadding(tab):
     # to jest w celu dostosowania bitow do nyquista i IFFT w OFDM
     # tab = [0] + tab
-    dl = len(tab)*30
+    tab[0]=0
+    dl = len(tab)*10
 
     for _ in range(dl):
         tab.append(0)
 
-dlugoscStrumienia = 20
+dlugoscStrumienia = 10
 
 bity = [generujDaneBinarne(dlugoscStrumienia),
         generujDaneBinarne(dlugoscStrumienia),
@@ -30,8 +36,10 @@ bity = [generujDaneBinarne(dlugoscStrumienia),
         generujDaneBinarne(dlugoscStrumienia),]
 
 out=[]
+zmodulowane = []
 for strumien in bity:
     zmodulowany = moduluj(strumien)
+    zmodulowane.append(zmodulowany)
     for x in zmodulowany:
         out.append(x)
 
@@ -40,4 +48,20 @@ plt.plot(np.real(out))
 
 plt.subplot(2,1,2)
 plt.plot(np.abs(np.fft.fft(out[:len(out)//2])))
+plt.show()
+
+
+# pojedyncze bity i nosne
+strumien = generujDaneBinarne(4)
+strumien[0]=0
+plt.subplot(2,1,1)
+plt.stem(strumien)
+
+for i,b in enumerate(strumien):
+    nosna = [0 for _ in range(80)]
+    nosna[i] = b
+    zmodulowanySymbol = fft.ifft(nosna)
+    plt.subplot(2,1,2)
+    plt.plot(zmodulowanySymbol)
+    
 plt.show()
